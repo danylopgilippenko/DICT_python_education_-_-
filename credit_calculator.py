5eea015080da0eee82dba6373e741effa5dbe386
danylophilippenko <114073197+danylopgilippenko@users.noreply.github.com>
2023-06-01 20:49:57 +0300

    Update credit_calculator.py

--- credit_calculator.py
+++ credit_calculator.py
@@ -1,76 +1,105 @@
+"""Credit Calculator Project"""
+
+# Libraries
 import math
 import argparse
 
 
-def periods_calc(inputted_principal, inputted_payment, inputted_interest):
-    calc_periods = math.ceil(math.log(inputted_payment / (inputted_payment - inputted_interest * inputted_principal), 1 + inputted_interest))
-    years = f'{calc_periods // 12} year{"s" * bool(calc_periods // 12 - 1)}' * bool(calc_periods // 12)
-    months = f'{calc_periods % 12} month{"s" * bool(calc_periods % 12 - 1)}' * bool(calc_periods % 12)
-    print(f'It will takes {years}{" and " * int(bool(months) and bool(years))}{months} to repay this loan!')
-    return calc_periods
+class CreditCalculator:
+    def __init__(self):
+        """Initialize the class"""
+        self.payment: float = 0
+        self.principal: float = 0
+        self.interest: float = 0
+        self.overpayment: float = 0
+        self.periods = 0
 
+    def annuity_mon_pay(self):
+        """Calculate annuity monthly payment"""
+        i = self.interest / (12 * 100)
+        a = math.ceil(self.principal * (i * pow(1 + i, self.periods)) / (pow(1 + i, self.periods) - 1))
+        print(f'Your annuity payment = {a}!')
+        self.overpayment = a * self.periods - self.principal
+        print(f'Overpayment = {self.overpayment}')
 
-def payment_calc(inputted_periods, inputted_principal, inputted_interest):
-    calc_payment = math.ceil(inputted_principal * ((inputted_interest * (1 + inputted_interest) ** inputted_periods) / ((1 + inputted_interest) ** inputted_periods - 1)))
-    print(f'Your monthly payment = {calc_payment}!')
-    return calc_payment
+    def diff_pay(self):
+        """Calculate differentiated monthly payment"""
+        i = self.interest / (12 * 100)
+        for m in range(1, self.periods + 1):
+            d = math.ceil(self.principal / self.periods + i *
+                          (self.principal - (self.principal * (m - 1)) / self.periods))
+            print(f'Month {m}: paid out {d}')
+            self.overpayment += d
+        print(f'Overpayment = {self.overpayment - self.principal}')
 
-
-def principal_calc(inputted_payment, inputted_periods, inputted_interest):
-    calc_principal = int(inputted_payment / ((inputted_interest * (1 + inputted_interest) ** inputted_periods) / ((1 + inputted_interest) ** inputted_periods - 1)))
-    print(f'Your loan principal = {calc_principal}!')
-    return calc_principal
-
-
-def diff_payment_calc(inputted_periods, inputted_principal, inputted_interest):
-    calc_months = [math.ceil(inputted_principal / inputted_periods + inputted_interest * (inputted_principal - (inputted_principal * (i - 1) / inputted_periods)))
-              for i in range(1, inputted_periods + 1)]
-    calc_overpayment = 0
-    for calc_month, inputted_payment in enumerate(calc_months):
-        print(f'Month {calc_month + 1}: payment is {inputted_payment}!')
-        calc_overpayment += inputted_payment
-    print(f'Overpayment {calc_overpayment - inputted_principal}')
-
-
-def annuity_overpayment_calc(inputted_payment, inputted_periods, inputted_principal):
-    return math.ceil(inputted_payment) * math.ceil(inputted_periods) - inputted_principal
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--type", help="type", )
-parser.add_argument("--principal", help="principal", type=int)
-parser.add_argument("--periods", help="periods", type=int)
-parser.add_argument("--interest", help="interest", type=float)
-parser.add_argument("--payment", help="payment", type=float)
-
-args = parser.parse_args()
-
-credit_type = args.type
-principal = args.principal
-periods = args.periods
-interest = args.interest
-payment = args.payment
-if credit_type not in ['annuity', 'diff'] or interest is None:
-    print('Incorrect parameters')
-elif credit_type == 'diff' and payment is not None or credit_type == 'diff' and None in [periods, principal]:
-    print('Incorrect parameters')
-elif credit_type == 'annuity' and [periods, principal, payment].count(None) > 1:
-    print('Incorrect parameters')
-elif credit_type == 'diff' and False in [periods >= 1, principal >= 1, interest >= 0]:
-    print('Incorrect parameters')
-else:
-    nominal_interest = interest / (12 * 100)
-    if credit_type == 'diff':
-        diff_payment_calc(periods, principal, inputted_interest=nominal_interest)
-    elif credit_type == 'annuity':
-        if periods is None:
-            periods = periods_calc(principal, payment, inputted_interest=nominal_interest)
-        elif payment is None:
-            payment = payment_calc(periods, principal, inputted_interest=nominal_interest)
-        elif principal is None:
-            principal = principal_calc(payment, periods, inputted_interest=nominal_interest)
+    def num_mon_pay(self):
+        """Calculate the number of months to repay the credit"""
+        i = self.interest / (12 * 100)
+        n = math.ceil(math.log(self.payment / (self.payment - i * self.principal), 1 + i))
+        years = n // 12
+        months = n % 12
+        self.overpayment = self.payment * n - self.principal
+        if years == 0:
+            print(f'You need {months} months to repay this credit!')
+        elif months == 0:
+            print(f'You need {years} years to repay this credit!')
         else:
-            print('Incorrect parameters ')
-        overpayment = annuity_overpayment_calc(payment, periods, principal)
-        print(f'Overpayment = {overpayment:.2f}')
-        
+            print(f'You need {years} years and {months} months to repay this credit!')
+        print(f'Overpayment = {self.overpayment}')
+
+    def loan_principal(self):
+        """Calculate the loan principal"""
+        i = self.interest / (12 * 100)
+        p = math.floor(self.payment / ((i * pow(1 + i, self.periods)) / (pow(1 + i, self.periods) - 1)))
+        print(f'Your loan principal = {p}!')
+        self.overpayment = self.payment * self.periods - p
+        print(f'Overpayment = {self.overpayment}')
+
+    def parser(self):
+        """Parse the arguments and call the corresponding method"""
+        if args.type == 'annuity':
+            if args.payment is not None and args.principal is not None and args.interest is not None:
+                self.payment = args.payment
+                self.principal = args.principal
+                self.interest = args.interest
+                self.num_mon_pay()
+            elif args.payment is not None and args.periods is not None and args.interest is not None:
+                self.payment = args.payment
+                self.periods = args.periods
+                self.interest = args.interest
+                self.loan_principal()
+            elif args.principal is not None and args.periods is not None and args.interest is not None:
+                self.principal = args.principal
+                self.periods = args.periods
+                self.interest = args.interest
+                self.annuity_mon_pay()
+            else:
+                print('Incorrect parameters')
+        elif args.type == 'diff':
+            if args.type is None or args.interest is None or args.principal is None \
+                    or args.periods is None:
+                print('Incorrect parameters')
+            elif args.principal and args.periods and args.interest:
+                self.principal = args.principal
+                self.periods = args.periods
+                self.interest = args.interest
+                self.diff_pay()
+            else:
+                print('Incorrect parameters')
+        else:
+            print('Incorrect parameters')
+
+
+# Main
+if __name__ == '__main__':
+    credit_calc = CreditCalculator()
+    parser = argparse.ArgumentParser()
+    # Add the arguments to the parser
+    parser.add_argument('--type', type=str, help='type of payment')
+    parser.add_argument('--payment', type=float, help='monthly payment')
+    parser.add_argument('--principal', type=float, help='credit principal')
+    parser.add_argument('--periods', type=int, help='the number of payment periods')
+    parser.add_argument('--interest', type=float, help='the loan interest rate')
+    args = parser.parse_args()
+    credit_calc.parser()
+    
